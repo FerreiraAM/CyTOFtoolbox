ea2f4e08956a191fc1e6d123bc1a63a24b680ed3
Anne-Maud Ferreira <amferrei@DN525jn1.SUNet>
2020-02-25 17:52:18 -0800

    remove the name in the legend, use directly the metacluster variable given as a parameter

--- R/CATALYST_clusters_plots.R
+++ R/CATALYST_clusters_plots.R
@@ -36,11 +36,12 @@ extract_SCE_info <- function(sce, metacluster, dr = "UMAP"){
 plot_metaclusters <- function(df, metacluster, point_size = 0.5, point_alpha = 0.5){
   ## CATALYST colors for meta-cluster 
   ## (keeping the cluster colors defined by the CATALYST package)
+  ## TODO redefine the colors within the package
   catalyst_col <- CATALYST:::.cluster_cols
   # Plot
   ggplot(df, aes_string(x = "X1", y = "X2")) +
     geom_point(aes_string(col = metacluster), size = point_size, alpha = point_alpha, pch = 16) +
-    scale_color_manual(values = catalyst_col, name = paste(metacluster, "meta-clusters")) +
+    scale_color_manual(values = catalyst_col, name = metacluster) +
     labs(x = paste(dr, "1"), y = paste(dr, "2")) + 
     theme_bw() + 
     guides(colour = guide_legend(override.aes = list(size = 5, alpha = 1)))
