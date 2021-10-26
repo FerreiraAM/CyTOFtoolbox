46427072e98780cdb6774307380ddeae6ef12d12
Anne-Maud Ferreira <amferrei@stanford.edu>
2021-10-26 15:51:29 -0700

    Update package description. Remove clustering section and functions not used for the cytoglmm-extension.

--- R/CyTOFtoolbox.R
+++ R/CyTOFtoolbox.R
@@ -1,23 +1,16 @@
-#' CyTOFtoolbox: A package regrouping tools to perform CyTOF data analysis.
+#' CyTOFtoolbox: A package regrouping visualization tools for CyTOF data analysis.
 #'
-#' The CyTOFtoolbox package regroups functions to perfom:
-#' differential expression analysis and clustering.
+#' The CyTOFtoolbox package regroups visualization tools for the analysis 
+#' of CyTOF data.
 #'
 #' @section CytoGLMM-extension:
 #' The volcano plot functions prepare the data to plot a volcano plot adapted for
 #' the CyTOF data. It uses the `CytoGLMM` output as well as the raw data.
 #' A modified MDS plot function is also available, based on the original one from
 #' the `CytoGLMM` package. It adds a threshold on the correlation marker/MDS axes.
-#' 
-#' @section Clustering: WARNING this section of the package is not present in this version 
-#' A modified heatmap plot of the differential abundance (DA) test results from 
-#' `CATALYST` package is available with the option of showing or not the sample
-#' IDs as well as predefining the colors if needed.
 #'
 #' @docType package
 #' @name CyTOFtoolbox
-#' 
-#' @importClassesFrom SummarizedExperiment SummarizedExperiment
 #'
 #' @import CytoGLMM
 #' @import ggplot2
@@ -26,6 +19,4 @@
 #' @importFrom ggrepel geom_text_repel
 #' @importFrom grid gpar
 #' @importFrom magrittr "%>%"
-#' @importFrom SummarizedExperiment assay colData rowData
-#' @importFrom S4Vectors metadata
 NULL
